fac8f68f7a19b925e9df4bc66be1980fa7080709
Vincent Untz <vuntz@novell.com>
2008-09-04 03:01:33 +0200

    Move all the code interacting with the website in a specific class.
    This way, we have some kind of API we can easily use. Makes it easier to
    add new subcommands.

--- osc-gnome.py
+++ osc-gnome.py
@@ -1,5 +1,144 @@
+class OscGnomeWebError(Exception):
+    def __init__(self, value):
+        self.msg = value
+
+    def __str__(self):
+        return repr(self.msg)
+
+
+#######################################################################
+
+
+class OscGnomeWeb:
+
+    _reserve_url = 'http://tmp.vuntz.net/opensuse-packages/reserve.py'
+    _csv_url = 'http://tmp.vuntz.net/opensuse-packages/obs.py?format=csv'
+
+    def __init__(self, exception):
+        self.Error = exception
+
+
+    def _parse_reservation(self, line):
+        try:
+            (package, username, comment) = line[:-1].split(';')
+            return (package, username, comment)
+        except ValueError:
+            print >>sys.stderr, 'Cannot parse reservation information: ' + line[:-1]
+            return (None, None, None)
+
+
+    def get_packages_versions(self):
+        packages_versions = []
+
+        try:
+            fd = urllib2.urlopen(self._csv_url)
+        except urllib2.HTTPError:
+            raise self.Error('Cannot get versions of packages: ' + e.msg)
+
+        lines = fd.readlines()
+        fd.close()
+
+        for line in lines:
+            try:
+                (package, oF_version, GF_version, upstream_version, empty) = line.split(';')
+                packages_versions.append((package, oF_version, GF_version, upstream_version))
+            except ValueError:
+                print >>sys.stderr, 'Cannot parse line: ' + line[:-1]
+                continue
+
+        return packages_versions
+
+
+    def get_reserved_packages(self, return_package = True, return_username = True, return_comment = False):
+        reserved_packages = []
+
+        try:
+            fd = urllib2.urlopen(self._reserve_url + '?mode=getall')
+        except urllib2.HTTPError, e:
+            raise self.Error('Cannot get list of reserved packages: ' + e.msg)
+
+        lines = fd.readlines()
+        fd.close()
+
+        # it returns a status code on the first line, and then one package per
+        # line
+        # if the status code is 200, then everything is good
+        if lines[0][:3] != '200':
+            raise self.Error('Error while getting list of reserved packages: ' + lines[0][4:-1])
+        else:
+            del lines[0]
+            for line in lines:
+                (package, username, comment) = self._parse_reservation(line)
+                if package:
+                    reserved_packages.append((package, username, comment))
+
+        if return_package and return_username and return_comment:
+            return reserved_packages
+        elif return_package and return_username and not return_comment:
+            retval = []
+            for (package, username, comment) in reserved_packages:
+                retval.append((package, username))
+            return retval
+        elif return_package and not return_username and not return_comment:
+            retval = []
+            for (package, username, comment) in reserved_packages:
+                retval.append(package)
+            return retval
+        else:
+            raise self.Error('Unsupported request for reserved packages. Please ask developers to implement it.')
+
+
+    def is_package_reserved(self, package):
+        try:
+            fd = urllib2.urlopen(self._reserve_url + '?mode=get&package=' + package)
+        except urllib2.HTTPError, e:
+            raise self.Error('Cannot look if package ' + package + ' is reserved: ' + e.msg)
+
+        line = fd.readline()
+        fd.close()
+
+        if line[:3] != '200':
+            raise self.Error('Cannot look if package ' + package + ' is reserved: ' + line[4:-1])
+
+        (package, username, comment) = self._parse_reservation(line[4:])
+
+        if not username or username == '':
+            return None
+        else:
+            return username
+
+
+    def reserve_package(self, package, username):
+        try:
+            fd = urllib2.urlopen(self._reserve_url + '?mode=set&user=' + username + '&package=' + package)
+        except urllib2.HTTPError, e:
+            raise self.Error('Cannot reserve package ' + package + ': ' + e.msg)
+
+        line = fd.readline()
+        fd.close()
+
+        if line[:3] != '200':
+            raise self.Error('Cannot reserve package ' + package + ': ' + line[4:-1])
+
+
+    def unreserve_package(self, package, username):
+        try:
+            fd = urllib2.urlopen(self._reserve_url + '?mode=unset&user=' + username + '&package=' + package)
+        except urllib2.HTTPError, e:
+            raise self.Error('Cannot unreserve package ' + package + ': ' + e.msg)
+
+        line = fd.readline()
+        fd.close()
+
+        if line[:3] != '200':
+            raise self.Error('Cannot unreserve package ' + package + ': ' + line[4:-1])
+
+
+#######################################################################
+
+
 # TODO: put this in a common library -- it's used in the examples too
-def _gnome_compare_versions_a_gt_b (self, a, b):
+def _gnome_compare_versions_a_gt_b(self, a, b):
     if self._gnome_rpm_module:
         # We're not really interested in the epoch or release parts of the
         # complete version because they're not relevant when comparing to
@@ -27,14 +166,6 @@ def _gnome_compare_versions_a_gt_b (self, a, b):
     return False
 
 
-def _gnome_parse_reservation(self, line):
-    try:
-        (package, username, comment) = line[:-1].split(';')
-        return (package, username, comment)
-    except ValueError:
-        print >>sys.stderr, 'Cannot parse reservation information: ' + line[:-1]
-        return (None, None, None)
-
 #######################################################################
 
 
@@ -45,7 +176,7 @@ def _gnome_todo(self, need_factory_sync, exclude_reserved):
         self._gnome_rpm_module = None
 
     # helper functions
-    def is_submitted (package, submitted_packages):
+    def is_submitted(package, submitted_packages):
         for submitted in submitted_packages:
             if package == submitted.dst_package:
                 return True
@@ -59,39 +190,21 @@ def _gnome_todo(self, need_factory_sync, exclude_reserved):
             print '%-32.32s | %-12.12s | %-12.12s' % (package, oF_version, GF_version)
 
 
-    # get the list of reserved package
-    reserved_packages = []
+    # get all versions of packages
     try:
-        fd = urllib2.urlopen(self._gnome_reserve_url + '?mode=getall')
-    except urllib2.HTTPError, e:
-        print >>sys.stderr, 'Cannot get list of reserved packages: ' + e.msg
+        packages_versions = self._gnome_web.get_packages_versions()
+    except self.OscGnomeWebError, e:
+        print >>sys.stderr, e.msg
+        return
 
-    lines = fd.readlines()
-    fd.close()
-
-    # it returns a status code on the first line, and then one package per line
-    # if the status code is 200
-    if lines[0][:3] != '200':
-        print >>sys.stderr, 'Error while getting list of reserved packages: ' + lines[0][4:-1]
-    else:
-        del lines[0]
-        for line in lines:
-            (package, username, comment) = self._gnome_parse_reservation (line)
-            if package:
-                reserved_packages.append(package)
+    # get the list of reserved package
+    try:
+        reserved_packages = self._gnome_web.get_reserved_packages(return_username = False)
+    except self.OscGnomeWebError, e:
+        print >>sys.stderr, e.msg
 
     # get the packages submitted to GNOME:Factory
     submitted_packages = get_submit_request_list(conf.config['apiurl'], 'GNOME:Factory', None)
-
-    # get the current versions of packages
-    try:
-        fd = urllib2.urlopen(self._gnome_csv_url)
-    except urllib2.HTTPError:
-        # FIXME: do we want a custom message?
-        raise
-
-    lines = fd.readlines()
-    fd.close()
 
     # print headers
     if need_factory_sync:
@@ -101,13 +214,7 @@ def _gnome_todo(self, need_factory_sync, exclude_reserved):
         print_package('Package', 'openSUSE:Factory', 'GNOME:Factory', 'Upstream')
         print '---------------------------------+--------------+--------------+-------------'
 
-    for line in lines:
-        try:
-            (package, oF_version, GF_version, upstream_version, empty) = line.split(';')
-        except ValueError:
-            print >>sys.stderr, 'Cannot parse line: ' + line[:-1]
-            continue
-
+    for (package, oF_version, GF_version, upstream_version) in packages_versions:
         if need_factory_sync:
             if self._gnome_compare_versions_a_gt_b(GF_version, oF_version):
                 print_package(package, oF_version, GF_version)
@@ -132,25 +239,15 @@ def _gnome_todo(self, need_factory_sync, exclude_reserved):
 
 def _gnome_listreserved(self):
     try:
-        fd = urllib2.urlopen(self._gnome_reserve_url + '?mode=getall')
-    except urllib2.HTTPError, e:
-        print >>sys.stderr, 'Cannot list reserved packages: ' + e.msg
+        reserved_packages = self._gnome_web.get_reserved_packages()
+    except self.OscGnomeWebError, e:
+        print >>sys.stderr, e.msg
         return
-
-    lines = fd.readlines()
-    fd.close()
-
-    if lines[0][:3] != '200':
-        print >>sys.stderr, 'Cannot list reserved packages: ' + lines[0][4:-1]
-        return
-
-    del lines[0]
 
     print '%-32.32s | %-12.12s' % ('Package', 'Reserved by')
     print '---------------------------------+-------------'
 
-    for line in lines:
-        (package, username, comment) = self._gnome_parse_reservation (line)
+    for (package, username) in reserved_packages:
         # FIXME 32 & 12 are arbitrary values. We should probably look at all
         # package names/versions and find the longer name/version
         if (package and username):
@@ -162,23 +259,12 @@ def _gnome_listreserved(self):
 
 def _gnome_isreserved(self, package):
     try:
-        fd = urllib2.urlopen(self._gnome_reserve_url + '?mode=get&package=' + package)
-    except urllib2.HTTPError, e:
-        print >>sys.stderr, 'Cannot look if package ' + package + ' is reserved: ' + e.msg
+        username = self._gnome_web.is_package_reserved(package)
+    except self.OscGnomeWebError, e:
+        print >>sys.stderr, e.msg
         return
 
-    line = fd.readline()
-    fd.close()
-
-    if line[:3] != '200':
-        print >>sys.stderr, 'Cannot look if package ' + package + ' is reserved: ' + line[4:-1]
-        return
-
-    (package, username, comment) = self._gnome_parse_reservation (line[4:])
     if not username:
-        return
-
-    if username == '':
         print 'Package is not reserved.'
     else:
         print 'Package ' + package + ' is reserved by ' + username + '.'
@@ -189,16 +275,9 @@ def _gnome_isreserved(self, package):
 
 def _gnome_reserve(self, package, username):
     try:
-        fd = urllib2.urlopen(self._gnome_reserve_url + '?mode=set&user=' + username + '&package=' + package)
-    except urllib2.HTTPError, e:
-        print >>sys.stderr, 'Cannot reserve package ' + package + ': ' + e.msg
-        return
-
-    line = fd.readline()
-    fd.close()
-
-    if line[:3] != '200':
-        print >>sys.stderr, 'Cannot reserve package ' + package + ': ' + line[4:-1]
+        self._gnome_web.reserve_package(package, username)
+    except self.OscGnomeWebError, e:
+        print >>sys.stderr, e.msg
         return
 
     print 'Package ' + package + ' reserved for 36 hours.'
@@ -211,16 +290,9 @@ def _gnome_reserve(self, package, username):
 
 def _gnome_unreserve(self, package, username):
     try:
-        fd = urllib2.urlopen(self._gnome_reserve_url + '?mode=unset&user=' + username + '&package=' + package)
-    except urllib2.HTTPError, e:
-        print >>sys.stderr, 'Cannot unreserve package ' + package + ': ' + e.msg
-        return
-
-    line = fd.readline()
-    fd.close()
-
-    if line[:3] != '200':
-        print >>sys.stderr, 'Cannot unreserve package ' + package + ': ' + line[4:-1]
+        self._gnome_web.unreserve_package(package, username)
+    except self.OscGnomeWebError, e:
+        print >>sys.stderr, e.msg
         return
 
     print 'Package ' + package + ' unreserved.'
@@ -276,8 +348,7 @@ def do_gnome(self, subcmd, opts, *args):
     if len(args) - 1 > max_args:
         raise oscerr.WrongArgs('Too many arguments.')
 
-    self._gnome_reserve_url = 'http://tmp.vuntz.net/opensuse-packages/reserve.py'
-    self._gnome_csv_url = 'http://tmp.vuntz.net/opensuse-packages/obs.py?format=csv'
+    self._gnome_web = self.OscGnomeWeb(self.OscGnomeWebError)
 
     # Do the command
     if cmd in ['todo', 't']:
