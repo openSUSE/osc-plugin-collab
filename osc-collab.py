9068265871b3ea43094c997030675af5a18068f4
Vincent Untz <vuntz@opensuse.org>
2010-01-17 20:03:01 +0100

    [osc-collab] Post-release bump to 0.92

--- osc-collab.py
+++ osc-collab.py
@@ -34,7 +34,7 @@
 # Authors: Vincent Untz <vuntz@opensuse.org>
 #
 
-OSC_COLLAB_VERSION = '0.91'
+OSC_COLLAB_VERSION = '0.92'
 
 # This is a hack to have osc ignore the file we create in a package directory.
 _osc_collab_helper_prefixes = [ 'osc-collab.', 'osc-gnome.' ]
