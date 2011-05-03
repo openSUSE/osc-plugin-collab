620cacdbdecef5e7e7c678dd856db7c8ac136bf0
Vincent Untz <vuntz@opensuse.org>
2011-05-04 00:47:19 +0200

    upstream: Move version comparisons functions to util.py
    This will be used elsewhere in obs-db, so we need to share that.
    
    Also add easier version_gt/version_ge API.

--- server/obs-db/util.py
+++ server/obs-db/util.py
@@ -65,3 +65,62 @@ def safe_unlink(filename):
     except OSError, e:
         if e.errno != errno.ENOENT:
             raise e
+
+
+########################################################
+
+
+# comes from convert-to-tarball.py
+def _strict_bigger_version(a, b):
+    a_nums = a.split('.')
+    b_nums = b.split('.')
+    num_fields = min(len(a_nums), len(b_nums))
+    for i in range(0,num_fields):
+        if   int(a_nums[i]) > int(b_nums[i]):
+            return a
+        elif int(a_nums[i]) < int(b_nums[i]):
+            return b
+    if   len(a_nums) > len(b_nums):
+        return a
+    elif len(a_nums) < len(b_nums):
+        return b
+    else:
+        return None
+
+
+def bigger_version(a, b):
+    # We compare versions this way (with examples):
+    #   + 0.3 and 0.3.1:
+    #     0.3.1 wins: 0.3 == 0.3 and 0.3.1 has another digit
+    #   + 0.3-1 and 0.3-2:
+    #     0.3-2 wins: 0.3 == 0.3 and 1 < 2
+    #   + 0.3.1-1 and 0.3-2:
+    #     0.3.1-1 wins: 0.3.1 > 0.3
+    a_nums = a.split('-')
+    b_nums = b.split('-')
+    num_fields = min(len(a_nums), len(b_nums))
+    for i in range(0,num_fields):
+        bigger = _strict_bigger_version(a_nums[i], b_nums[i])
+        if   bigger == a_nums[i]:
+            return a
+        elif bigger == b_nums[i]:
+            return b
+    if len(a_nums) > len(b_nums):
+        return a
+    else:
+        return b
+
+
+def version_gt(a, b):
+    if a == b:
+        return False
+
+    bigger = bigger_version(a, b)
+    return a == bigger
+
+def version_ge(a, b):
+    if a == b:
+        return True
+
+    bigger = bigger_version(a, b)
+    return a == bigger
