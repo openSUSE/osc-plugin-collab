f4aefec558c3e874edc55b746049fce119d130dc
Marcus Huewe <suse-tux@gmx.de>
2013-06-25 20:28:58 +0200

    osc-collab: Adapt code to osc's new plugin mechanism
    It still works with the old plugin mechanism.

--- osc-collab.py
+++ osc-collab.py
@@ -34,6 +34,8 @@
 # Authors: Vincent Untz <vuntz@opensuse.org>
 #
 
+from osc import cmdln
+
 OSC_COLLAB_VERSION = '0.97'
 
 # This is a hack to have osc ignore the file we create in a package directory.
@@ -43,14 +45,14 @@ for suffix in [ 'NEWS', 'ChangeLog', 'configure' ]:
     for prefix in _osc_collab_helper_prefixes:
         _osc_collab_helpers.append(prefix + suffix)
 
-try:
-    import conf
-    for helper in _osc_collab_helpers:
-        conf.DEFAULTS['exclude_glob'] += ' %s' % helper
-except:
-    # compatibility with osc <= 0.121
-    for helper in _osc_collab_helpers:
-        exclude_stuff.append(helper)
+def init():
+    try:
+        for helper in _osc_collab_helpers:
+            conf.DEFAULTS['exclude_glob'] += ' %s' % helper
+    except:
+        # compatibility with osc <= 0.121
+        for helper in _osc_collab_helpers:
+            exclude_stuff.append(helper)
 
 
 class OscCollabError(Exception):
@@ -3998,6 +4000,8 @@ def do_collab(self, subcmd, opts, *args):
     #self.gtime = self.OscCollabImport.m_import('time')
     #self.gref = self.gtime.time()
     #print "%.3f - %s" % (self.gtime.time()-self.gref, 'start')
+
+    init()
 
     self._osc_collab_alias = self.lastcmd[0]
 
