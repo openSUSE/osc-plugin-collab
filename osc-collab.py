8341c8d1ac78b4407807bcfd827476e729cabee1
Vincent Untz <vuntz@opensuse.org>
2019-05-03 16:27:31 +0200

    osc-collab: Post-release bump to 0.102

--- osc-collab.py
+++ osc-collab.py
@@ -69,7 +69,7 @@ from osc import cmdln
 from osc import conf
 
 
-OSC_COLLAB_VERSION = '0.101'
+OSC_COLLAB_VERSION = '0.102'
 
 # This is a hack to have osc ignore the file we create in a package directory.
 _osc_collab_helper_prefixes = [ 'osc-collab.', 'osc-gnome.' ]
